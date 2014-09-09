d924d33869ab86036d03c3340bfaa47e9ef0a4e8
Tuomas Tynkkynen <tuomas.tynkkynen@iki.fi>
2014-09-09 03:21:34 +0300

    Create project component.

---
+++ src/fi/iki/dezgeg/tmc/idea/integration/TmcProjectComponent.java
@@ -0,0 +1,34 @@
+package fi.iki.dezgeg.tmc.idea.integration;
+
+import com.intellij.openapi.components.ProjectComponent;
+import com.intellij.openapi.project.Project;
+import org.jetbrains.annotations.NotNull;
+
+public class TmcProjectComponent implements ProjectComponent {
+    private Project project;
+
+    public TmcProjectComponent(Project project) {
+        this.project = project;
+    }
+
+    public void initComponent() {
+        // TODO: insert component initialization logic here
+    }
+
+    public void disposeComponent() {
+        // TODO: insert component disposal logic here
+    }
+
+    @NotNull
+    public String getComponentName() {
+        return "TMC.ProjectComponent";
+    }
+
+    public void projectOpened() {
+        System.out.println("Hello world from TmcProjectComponent: " + project);
+    }
+
+    public void projectClosed() {
+        // called when project is being closed
+    }
+}
